4500fcb9621588786e7a4f459386bb93f4bcd6c6
1715leekc <1715leekc@naver.com>
2023-10-10 21:43:24 +0900

    Feat: chart layout

--- project/src/components/chart/chart.presenter.tsx
+++ project/src/components/chart/chart.presenter.tsx
@@ -2,9 +2,21 @@ import styles from './chart.module.css'
 
 export default function ChartPageUI() {
     return (
-        <div className={styles.wrapper}>
-            sasdasd
+        <div className={styles.body}>
+            <div className={styles.wrapper}>
+                <div className={styles.containers}>
+                    <div className={styles.chartContainer}>
+                        ss
+                    </div>
+                    <div className={styles.newsContainer}>
+                        asd
+                    </div>
+                </div>
+            </div>
+            <div className={styles.footer}>
+                asdasd
+            </div>
         </div>
-
+        
     )
 }
