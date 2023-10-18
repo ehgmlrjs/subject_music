877eec7be000ab61a291e993015091e390bff149
ehgmlrjs <ehgmlrjs@naver.com>
2023-10-18 14:42:16 +0900

    comment
    comment

--- server/routes/contents.js
+++ server/routes/contents.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const database = require('../config/database.config');
 
-router.get('/:index', async (req, res, next) => {
+router.get('/:index', async (req, res) => {
     let co;
     try {
         const index = req.params.index;
@@ -22,5 +22,32 @@ router.get('/:index', async (req, res, next) => {
         });
     }
 })
+
+router.get('/:index/board', async(req,res) => {
+    let co;
+
+    try{
+        const {nickname,comment} = req.body;
+        const index = req.params.index;
+        console.log(nickname)
+        console.log(comment)
+        console.log(index)
+        co = await database.getConnection();
+        const query = 'INSERT INTO laproject.board (`Index`,nickname, comment)  VALUES (?, ?, ?)';
+        const values = [index,nickname,comment];
+
+        const [result] = await co.execute(query, values);
+        co.release();
+        res.send(result)
+        return result;
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
+})
+
+// router.get('/:index/b')
 
 module.exports = router;
