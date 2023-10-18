b4264f85935c71fc87af89ea988c73ae7b7f0549
ehgmlrjs <ehgmlrjs@naver.com>
2023-10-18 15:03:01 +0900

    board
    board

--- server/routes/contents.js
+++ server/routes/contents.js
@@ -23,22 +23,17 @@ router.get('/:index', async (req, res) => {
     }
 })
 
-router.get('/:index/board', async(req,res) => {
+router.post('/:index/board', async(req,res) => {
     let co;
 
     try{
-        const {nickname,comment} = req.body;
         const index = req.params.index;
-        console.log(nickname)
-        console.log(comment)
-        console.log(index)
         co = await database.getConnection();
-        const query = 'INSERT INTO laproject.board (`Index`,nickname, comment)  VALUES (?, ?, ?)';
-        const values = [index,nickname,comment];
+        const query = 'SELECT * FROM laproject.board WHERE `Index`=?';
+        const values = [index];
 
         const [result] = await co.execute(query, values);
         co.release();
-        res.send(result)
         return result;
     } catch (error) {
         console.error(error)
@@ -48,6 +43,45 @@ router.get('/:index/board', async(req,res) => {
     }
 })
 
-// router.get('/:index/b')
+router.post('/:index/boardUpdate', async(req,res) => {
+    let co;
+
+    try{
+        const {nickname,comment} = req.body;
+        const index = req.params.index;
+        co = await database.getConnection();
+        const query = 'INSERT INTO laproject.board (`Index`,nickname, comment)  VALUES (?, ?, ?)';
+        const values = [index,nickname,comment];
+
+        const [result] = await co.execute(query, values);
+        co.release();
+        return result;
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
+})
+
+router.post('/:index/boardDelete', async(req,res) =>{
+    let co;
+    try{
+        const {nickname,comment} = req.body;
+        const index = req.params.index;
+        co = await database.getConnection();
+        const query = 'DELETE FROM laproject.board WHERE `INDEX`=? AND nickname=? AND comment=?';
+        const values = [index,nickname,comment];
+
+        const [result] = await co.execute(query, values);
+        co.release();
+        return result;
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
+})
 
 module.exports = router;
