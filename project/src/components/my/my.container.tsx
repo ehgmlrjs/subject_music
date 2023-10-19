0698438493b8d12ab1865ab2dc9d52b0e2bc5dcf
1715leekc <1715leekc@naver.com>
2023-10-19 12:07:10 +0900

    Feat: mupage fetchdata

--- project/src/components/my/my.container.tsx
+++ project/src/components/my/my.container.tsx
@@ -1,9 +1,33 @@
 import MyPageUI from "./my.presenter";
 import { useRecoilState } from "recoil";
 import { nickState } from "@/src/store/states";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function MyPage() {
     const [localNick, setLocalNick] = useRecoilState(nickState)
+
+    const fetchData = async () => {
+        try {
+            const token = localStorage.getItem('token')
+
+            const response = await axios.post('http://localhost:8080/mypage/',{
+            nickname : localNick
+            },{
+                headers: {
+                    Authorization: `${token}`
+                }
+            })
+            console.log(response)
+        }catch(error){
+            console.log('error',error)
+        }  
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
 
     return(
         <MyPageUI
